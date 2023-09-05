99df19609bf864410250ca024868cc67bbc139d7
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-05 17:01:41 +0800

    Create RsaAuthorizeFilterAttribute

---
+++ src/UserCenter.Application/Attributes/RsaAuthorizeFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace UserCenter.Application.Filters
+{
+    public class RsaAuthorizeFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            return base.OnActionExecutionAsync(context, next);
+        }
+    }
+}
