33af66a3d9f861c9b2a5a5465db905dae10939a3
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-30 23:06:56 +0800

    Update Grpc

--- src/UserCenter.Application/GrpcProviders/UserGrpcProvider.cs
+++ src/UserCenter.Application/GrpcProviders/UserGrpcProvider.cs
@@ -1,8 +1,22 @@
-﻿using UserCenter.GrpcServices;
+﻿using Grpc.Core;
+using UserCenter.GrpcServices;
 
 namespace UserCenter.Application.GrpcProviders
 {
-    public class UserGrpcProvider : UserGrpcService
+    public class UserGrpcProvider : UserGrpcService.UserGrpcServiceBase
     {
+        public UserGrpcProvider()
+        {
+        }
+
+        public override Task<UserResponse> Login(LoginRequest request, ServerCallContext context)
+        {
+            return base.Login(request, context);
+        }
+
+        public override Task<UserResponse> GetUser(GetUserRequest request, ServerCallContext context)
+        {
+            return base.GetUser(request, context);
+        }
     }
 }
